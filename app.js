af42661dd8a24fe3f734f46e03d08e00b1be244c
nkhlrgh <43105834+nkhlrgh@users.noreply.github.com>
2022-02-16 23:04:14 +0530

    Add frog movement controls

---
+++ app.js
@@ -0,0 +1,32 @@
+const timeLeftDisplay = document.querySelector("#time-left");
+const resultDisplay = document.querySelector("#results");
+const StartPauseButton = document.querySelector("#start-pause-button");
+const squares = document.querySelectorAll(".grid div");
+let currentIndex = 76;
+const width = 9;
+
+function moveFrog(e) {
+  squares[currentIndex].classList.remove("frog");
+
+  switch (e.key) {
+    case "ArrowLeft":
+      console.log("move-left");
+      currentIndex -= 1;
+      break;
+    case "ArrowRight":
+      console.log("move-right");
+      currentIndex += 1;
+      break;
+    case "ArrowUp":
+      console.log("move-up");
+      currentIndex -= width;
+      break;
+    case "ArrowDown":
+      console.log("move-down");
+      currentIndex += width;
+      break;
+  }
+
+  squares[currentIndex].classList.add("frog");
+}
+document.addEventListener("keyup", moveFrog);
